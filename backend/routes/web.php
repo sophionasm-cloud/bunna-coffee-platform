58a7f4840c387ee60a55610ab51db5cb63dab1b9
sophionasm-cloud <sophionasm@gmail.com>
2026-06-25 15:15:27 +0300

    Add debug and test route in web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Test route to verify Laravel is working
+Route::get('/hello', function () {
+    return ['message' => 'Hello from Laravel!'];
+});
